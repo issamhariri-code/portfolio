102a5d8e16d3101648e6cc2f1ccac86ba9cb5255
Issam Hariri <issamhariri@outlook.com>
2025-09-19 12:35:34 +0200

    Fortsatt bygga på menyn

--- pythonprojekt/to_do_app.py
+++ pythonprojekt/to_do_app.py
@@ -1,8 +1,8 @@
 def to_do_app():
     uppgifter = [
-        {"titel": "Läxor", "Kategori": "Studier", "klar": False},
-        {"titel": "Handla", "Kategori": "Mathandel", "klar": False},
-        {"titel": "Träning", "Kategori": "MMA", "klar": False}
+        {"Titel": "Läxor:", "Kategori": "Studier", "klar": False},
+        {"Titel": "Handla:", "Kategori": "Mathandel", "klar": False},
+        {"Titel": "Träning:", "Kategori": "MMA", "klar": False}
     ]
 
     print("Menyval:\n1. Visa alla uppgifter\n2. Lägg till ny uppgift\n3. Markera som klar\n4. Avsluta")
@@ -13,5 +13,28 @@ def to_do_app():
             for f in uppgifter:
                 f["Kategori"]
                 print (f["Kategori"])
+
+        elif val == 3:
+            if uppgifter == []:
+                print("Inga uppgifter att markera")
+            else:
+                for f in uppgifter:
+                    print (f["Titel"], f["Kategori"])
+            try:
+                val_nummer = int(input("Vilken uppgift vill du markera som klar? Ange numret:"))
+                if val_nummer < 1 or val_nummer > len(uppgifter):
+                    print("Ange korrekt alterntiv")
+            except ValueError:
+                print("Ange en siffra")
+
+            index = val_nummer -1
+            if uppgifter[index]["klar"]:
+                print("Redan klar")
+
+            else:
+                uppgifter[index]["klar"] = True
+                print("Markerad som klar!")
+
+                            
 
 to_do_app()
