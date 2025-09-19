fbedac7faa3a1a5757b67bf8c22cd182aefdbeb9
Issam Hariri <issamhariri@outlook.com>
2025-09-19 22:54:04 +0200

    la till continue så att användaren hoppar tillbaka till menyn efter ett val

--- pythonprojekt/to_do_app.py
+++ pythonprojekt/to_do_app.py
@@ -5,35 +5,45 @@ def to_do_app():
         {"Titel": "Träning:", "Kategori": "MMA", "klar": False}
     ]
 
-    print("Menyval:\n1. Visa alla uppgifter\n2. Lägg till ny uppgift\n3. Markera som klar\n4. Avsluta")
 
     while True:
+        print("Menyval:\n1. Visa alla uppgifter\n2. Lägg till ny uppgift\n3. Markera som klar\n4. Avsluta")
         val = int(input("Välj ett alternativ: "))
         if val == 1:
             for f in uppgifter:
-                f["Kategori"]
-                print (f["Kategori"])
+                if f["klar"]:
+                    status = "[x]"
+                else:
+                    status = "[ ]"
+                print (status, f["Titel"], f["Kategori"])
 
         elif val == 3:
-            if uppgifter == []:
+            if uppgifter == [ ]:
                 print("Inga uppgifter att markera")
+                continue
             else:
                 for f in uppgifter:
                     print (f["Titel"], f["Kategori"])
-            try:
-                val_nummer = int(input("Vilken uppgift vill du markera som klar? Ange numret:"))
-                if val_nummer < 1 or val_nummer > len(uppgifter):
-                    print("Ange korrekt alterntiv")
-            except ValueError:
-                print("Ange en siffra")
+                try:
+                    val_nummer = int(input("Vilken uppgift vill du markera som klar? Ange numret:"))
+                    if val_nummer < 1 or val_nummer > len(uppgifter):
+                        print("Ange korrekt alterntiv")
+                        continue
+                except ValueError:
+                    print("Ange en siffra")
+                    continue
 
-            index = val_nummer -1
-            if uppgifter[index]["klar"]:
-                print("Redan klar")
+                index = val_nummer -1
+                if uppgifter[index]["klar"]:
+                    print("Redan klar")
 
-            else:
-                uppgifter[index]["klar"] = True
-                print("Markerad som klar!")
+                else:
+                    uppgifter[index]["klar"] = True
+                    print("Markerad som klar!")
+
+        elif val == 4:
+            print("Vi ses nästa gång")
+            break
 
                             
 
