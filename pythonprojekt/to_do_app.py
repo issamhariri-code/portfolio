1bc64d8f888c315408d4860ab7898e4230fa6d30
Issam Hariri <issamhariri@outlook.com>
2025-09-19 23:54:17 +0200

    to_do_app.py

--- pythonprojekt/to_do_app.py
+++ pythonprojekt/to_do_app.py
@@ -5,46 +5,45 @@ def to_do_app():
         {"Titel": "Träning:", "Kategori": "MMA", "klar": False}
     ]
 
-
     while True:
         print("Menyval:\n1. Visa alla uppgifter\n2. Lägg till ny uppgift\n3. Markera som klar\n4. Avsluta")
         val = int(input("Välj ett alternativ: "))
+
         if val == 1:
             for f in uppgifter:
                 if f["klar"]:
                     status = "[x]"
                 else:
                     status = "[ ]"
-                print (status, f["Titel"], f["Kategori"])
+                print(status, f["Titel"], f["Kategori"])
+            input("Tryck Enter för att fortsätta...")
 
         elif val == 3:
-            if uppgifter == [ ]:
+            if not uppgifter:
                 print("Inga uppgifter att markera")
                 continue
             else:
                 for f in uppgifter:
-                    print (f["Titel"], f["Kategori"])
+                    print(f["Titel"], f["Kategori"])
                 try:
                     val_nummer = int(input("Vilken uppgift vill du markera som klar? Ange numret:"))
                     if val_nummer < 1 or val_nummer > len(uppgifter):
-                        print("Ange korrekt alterntiv")
+                        print("Ange korrekt alternativ")
                         continue
                 except ValueError:
                     print("Ange en siffra")
                     continue
 
-                index = val_nummer -1
+                index = val_nummer - 1
                 if uppgifter[index]["klar"]:
                     print("Redan klar")
-
                 else:
                     uppgifter[index]["klar"] = True
                     print("Markerad som klar!")
+            input("Tryck Enter för att fortsätta...")
 
         elif val == 4:
             print("Vi ses nästa gång")
             break
-
-                            
 
 to_do_app()
