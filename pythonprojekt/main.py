920b1a17535fd87f1b94e6994a3ca0b0e3b94332
Issam Hariri <issamhariri@outlook.com>
2025-10-01 14:58:52 +0200

    Avslutat stridsfunktionen samt med funktion om man vill spela igen, löst små buggar där jag inte hade lagt in break under vissa looper, importerat json, och datetime för att spara statistik i en JSON fil efter avslutad strid

--- pythonprojekt/main.py
+++ pythonprojekt/main.py
@@ -1,6 +1,11 @@
 from pokemon import Pokemon
 import random
 from menu import print_menu, get_choice
+from datetime import datetime
+from pathlib import Path
+import json
+
+results: list[dict] = []
 
 pokemon1 = Pokemon("Pikachu", 12, 60, 8)
 pokemon2 = Pokemon("Bulbasaur", 8, 70, 6)
@@ -10,46 +15,49 @@ pokemon4 = Pokemon("Charmander", 13, 55, 8)
 pokemon_list = [pokemon1, pokemon2, pokemon3, pokemon4]
 
 def starta_strid(pokemon_list):
-    lista_pokemon(pokemon_list)
-    val = int(input("Välj en siffra för att välja en Pokemon. "))
-    if val < 1 or val > len(pokemon_list):
-        print("Fel, pröva igen.")
-        return
-    index = val - 1
+    while True:
+        lista_pokemon(pokemon_list)
+        val = int(input("Välj en siffra för att välja en Pokemon. "))
+        if val < 1 or val > len(pokemon_list):
+            print("Fel, pröva igen.")
+            return
+        index = val - 1
+        original_player = pokemon_list[index]
+        original_enemy = random.choice(pokemon_list)
 
-    enemy_pokemon = random.choice(pokemon_list)
-    copy_pokemon = Pokemon(enemy_pokemon.namn, 
-                        enemy_pokemon.attack, 
-                        enemy_pokemon.hp,
-                        enemy_pokemon.varians)
-    copy_pokemon.max_hp = enemy_pokemon.max_hp
+        player_pokemon = original_player.copy_pokemon()
+        enemy_pokemon  = original_enemy.copy_pokemon()
 
-    player_pokemon = pokemon_list[index]
-    print(f"Du valde: {player_pokemon.namn}\n(Attack: {player_pokemon.attack}, Hälsa: {player_pokemon.hp} Varians: {Pokemon.varians})")
-    print(f"Motståndaren är: {enemy_pokemon}\n(Attack: {enemy_pokemon.attack}, Hälsa: {enemy_pokemon.hp}) Varians: {Pokemon.varians})")
+        print(f"Du valde {player_pokemon.namn} (Attack: {player_pokemon.attack}, Hälsa: {player_pokemon.hp}, Varians: {player_pokemon.varians})")
+        print(f"Motståndaren är: {enemy_pokemon.namn}, (Attack: {enemy_pokemon.attack}, Hälsa: {enemy_pokemon.hp}, Varians: {enemy_pokemon.varians})")
 
-    while (player_pokemon) and (enemy_pokemon):
 
-        """Användaren (spelaren som spelar)"""
-        skada = player_pokemon.intervall()
-        enemy_pokemon.hp = enemy_pokemon.hp - skada
-        enemy_pokemon.klipp_hp()
-        print(f"{player_pokemon} slog {enemy_pokemon} för {skada}. HP kvar: {enemy_pokemon.hp}")
+        
+        while player_pokemon.is_alive() and enemy_pokemon.is_alive():
+            #spelarens tur
+            slumpa_skada = player_pokemon.intervall()
+            enemy_pokemon.hp -= slumpa_skada
+            enemy_pokemon.klipp_hp()
+            print(f"{player_pokemon.namn} slog {enemy_pokemon.namn} för {slumpa_skada} HP kvar: {enemy_pokemon.hp}/{enemy_pokemon.max_hp}")
+            if enemy_pokemon.hp == 0:
+                print(f"{player_pokemon.namn} Vann striden !")
+                break
+            #motståndarens tur
+            slumpa_skada = enemy_pokemon.intervall()
+            player_pokemon.hp -= slumpa_skada
+            player_pokemon.klipp_hp()
+            print(f"{enemy_pokemon.namn} slog {player_pokemon.namn} för {slumpa_skada} HP kvar: {player_pokemon.hp}/{player_pokemon.max_hp}")
+            if player_pokemon.hp == 0:
+                print(f"{enemy_pokemon.namn} Vann striden !")
+                break
 
-        if (enemy_pokemon.hp <= 0):
-            print(f"{player_pokemon} Vann striden!")
-            break
-
-        """ Motståndaren som spelar (datorn)"""
-        skada = enemy_pokemon.intervall()
-        player_pokemon.hp = player_pokemon.hp - skada
-        player_pokemon.klipp_hp()
-        print(f"{enemy_pokemon} slog {player_pokemon} för {skada}. HP kvar: {player_pokemon.hp}")
-
-        if (player_pokemon.hp <= 0):
-            print(f"{enemy_pokemon} Vann striden!")
-            break
-
+        # Striden är slut här (någon vann). Fråga om användaren vill spela igen.
+        question = input("Vill du spela igen? svara med ett (Y / N): ")
+        svar = question.strip().lower()
+        if svar == "y":
+            continue
+        else:
+            return
 
 def meny_loop():
     while True:
@@ -62,7 +70,7 @@ def meny_loop():
         elif val == "3":
             ta_bort_pokemon(pokemon_list)
         elif val == "4":
-            print("Starta en strid. ")
+            starta_strid(pokemon_list)
         elif val == "5":
             print("Spara resultat (JSON)")
         elif val == "6":
