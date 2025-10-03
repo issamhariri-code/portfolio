fe5bd4bb203562a2cd72080864cf3d4812bdced6
Issam Hariri <issamhariri@outlook.com>
2025-10-03 14:57:54 +0200

    ändrat lite i meny filen

--- pythonprojekt/menu.py
+++ pythonprojekt/menu.py
@@ -5,7 +5,7 @@ def print_menu():
     print("1) Lista Pokemon. ")
     print("2) Lägg till Pokemon. ")
     print("3) Ta bort Pokemon. ")
-    print("4) Starta strid. ")
+    print("4) Starta strid <--- ")
     print("5) Spara resultat (JSON) ")
     print("6) Läs resultat (JSON) ")
     print("0) Avsluta. ")
